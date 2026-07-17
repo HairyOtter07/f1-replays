570612adeff908bb9a20de50ac7c01892e11fe16
HairyOtter07 <hairyotter07@gmail.com>
2026-07-16 18:41:42 -0700

    update copyright year

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="flex flex-col bg-zinc-200 w-full space-y-2 px-12 py-6 text-zinc-800 text-sm">
-    <p>This website is unofficial and is not associated in any way with the Formula 1 companies. F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE WORLD CHAMPIONSHIP, GRAND PRIX and related marks are trade marks of Formula One Licensing B.V.</p>
-    <p>© 2024 HairyOtter07</p>
-  </div>
+    <div
+        class="flex flex-col bg-zinc-200 w-full space-y-2 px-12 py-6 text-zinc-800 text-sm"
+    >
+        <p>
+            This website is unofficial and is not associated in any way with the
+            Formula 1 companies. F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE
+            WORLD CHAMPIONSHIP, GRAND PRIX and related marks are trade marks of
+            Formula One Licensing B.V.
+        </p>
+        <p>© 2026 HairyOtter07</p>
+    </div>
 </template>
